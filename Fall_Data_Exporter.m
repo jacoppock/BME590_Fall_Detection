9fba55c5baf7f46ebdb8fde54d8e0cb35277f086
James Coppock <jcoppock97@gmail.com>
2020-09-29 10:22:23 -0400

    Graphics update
    Accelerometry graphic update

--- Fall_Data_Exporter.m
+++ Fall_Data_Exporter.m
@@ -1,10 +1,11 @@
 %% Falling Dataset Labeling
+Path = '/Users/James/Desktop/Duke/Class/Fall 2020/Biomedical DS/ARS DLR Data Set/ARS_DLR_DataSet.mat';
+Data = struct2cell(load(Path, '-mat')); 
 Out = [];
  for jj = 1:length(Data)
-
- Data = [{Dat1},{Dat2},{Dat3}];
-
  Vec = zeros(length(Data{jj}{1}),1);
+ startInd = [];
+ stopInd = [];
  for ii = 1:length(Data{jj}{3})
      if strcmp(Data{jj}{3}{ii},'FALLING')
          disp(['found a fall'])
@@ -13,13 +14,25 @@ Out = [];
      else
      end
  end
- Vec(startInd:stopInd) = 1;
- 
+if ~isempty(startInd)
+Vec(startInd:stopInd) = 1;
+else
+end
 Vec = logical(Vec);
-
+%%%
+for i = 2:10
+% Regularize Variables
+Data{jj}{1}(:,i) = (Data{jj}{1}(:,i) - mean(Data{jj}{1}(:,i)));
+end
 Out = [Out;[Data{jj}{1} ,Vec]];
+
 end
  T = array2table(Out,...
     'VariableNames',{'Time','accX','accY','accZ','angX','angY','angZ','maxX','magY','magZ','Target'});
-filePath = fullfile(pwd,'Fall_Data.xlsx');
-writetable(T,filePath)
+[len col] = size(T);
+% Tout2 = T(round(len/2):end,:);
+% Tout = T(1:round(len/2)-1,:);
+filePath1 = fullfile(pwd,'Fall_Data.csv');
+
+writetable(T,filePath1)
+writetable(Tout2,filePath2)
